c8027a63ab4b220238eba3b755b567852d60397c
Arsen <52761327+cly4auHo@users.noreply.github.com>
2020-02-24 00:20:19 +0200

    Update BankApp.java

--- BankApp.java
+++ BankApp.java
@@ -5,7 +5,7 @@ public class BankApp {
     public static void main(String[] args) {
 
         Account<String> from = new Account1<>(2000, "12367");
-        Account<String> to = new Account2<>(1000, "absic1488");
+        Account<String> to = new Account2<>(1000, "absic14");
 
         Transaction<Account<String>> tr = new Transaction<>(from, to, 500);
         tr.execute();
